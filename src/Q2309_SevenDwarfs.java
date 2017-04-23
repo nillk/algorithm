bd7ae217067cd3e7212a1717bb61eb0cc2329a3f
nillk <hyunjoo88.kim@gmail.com>
2017-04-23 16:49:58 +0900

    pass

---
+++ src/Q2309_SevenDwarfs.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q2309_SevenDwarfs {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        byte[] heights = new byte[9];
+        short total = 0;
+        for (int i = 0; i < 9; i++) {
+            heights[i] = scan.nextByte();
+            total += heights[i];
+        }
+
+        Arrays.sort(heights);
+
+        int i = 0;
+        int j = 0;
+
+        FIND:
+        for (; i < 9; i++) {
+            for (j = 0; j < i; j++) {
+                if (total - (heights[i] + heights[j]) == 100) {
+                    break FIND;
+                }
+            }
+        }
+
+        for (int k = 0; k < 9; k++) {
+            if (k != i && k != j) System.out.println(heights[k]);
+        }
+
+        scan.close();
+    }
+}
