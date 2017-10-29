9c0b19440b7b77db0122c80d71e6250818aa05cd
nillk <hyunjoo88.kim@gmail.com>
2017-10-29 17:28:14 +0900

    solve

--- src/Q1463_MakeOne.java
+++ src/Q1463_MakeOne.java
@@ -1,44 +1,23 @@
 import java.util.Scanner;
 
 public class Q1463_MakeOne {
-	private static int[] cache;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int X = scan.nextInt();
-		cache = new int[X + 1];
-		
-		System.out.println(solve(X));
-		scan.close();
-	}
+        int X = scan.nextInt();
+        int[] cache = new int[X + 1];
+        cache[0] = 0;
+        cache[1] = 0;
 
-	private static int solve(int x) {
-		if (x == 0) return 0;
-		if (x == 1 || x == 2 || x == 3) return 1;
-		
-		if (cache[x] != 0) return cache[x];
+        for (int i = 2; i <= X; i++) {
+            cache[i] = cache[i - 1] + 1;
+            if (i % 3 == 0 && cache[i] > cache[i / 3] + 1)
+                cache[i] = cache[i / 3] + 1;
+            if (i % 2 == 0 && cache[i] > cache[i / 2] + 1)
+                cache[i] = cache[i / 2] + 1;
+        }
 
-		int oneLess = solve(x - 1);
-		int div2 = Integer.MAX_VALUE;
-		int div3 = Integer.MAX_VALUE;
-		if (x % 2 == 0) {
-			div2 = solve(x/2);
-		}
-		if (x % 3 == 0) {
-			div3 = solve(x/3);
-		}
-		
-		return cache[x] = 1 + min(oneLess, div2, div3);
-	}
-	
-	private static int min(int a, int b, int c) {
-		if (a < b) {
-			if (a < c) return a;
-			else return c;
-		} else { // a > b
-			if (b < c) return b;
-			else return c;
-		}
-	}
+        System.out.println(cache[X]);
+        scan.close();
+    }
 }
